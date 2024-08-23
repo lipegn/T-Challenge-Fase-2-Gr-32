b81b4c1d14adb0830e967bbda04dfed12d06ee0b
Felipe Gonçalves <lipegn2@gmail.com>
2024-08-23 11:10:16 -0300

    Ajuste caminho API para projeto de teste e sincronização da branch

--- Test/Integração/IntegracaoAPI.cs
+++ Test/Integração/IntegracaoAPI.cs
@@ -1,24 +1,15 @@
 ﻿using Core.Entity;
 using Core.Input;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization.Metadata;
-using System.Threading.Tasks;
-using TechChallangeCadastroContatosAPI.Controllers;
 
 namespace Test.Integração
 {
     public class IntegracaoAPI
     {
         HttpClient client = new();
-        const string URL_API = "https://localhost:7259/";
+        const string URL_API = "http://localhost/contatos/";
 
         async Task<string> BuscarToken()
         {
