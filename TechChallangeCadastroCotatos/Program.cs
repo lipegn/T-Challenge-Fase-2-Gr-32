cc43153ff9bd267df95851cfffecdf575333ea66
Mayara Alves da Silva <mayara.silva28@gmail.com>
2025-02-01 23:27:55 -0300

    adiciona configuração de variavel de ambiente

--- TechChallangeCadastroCotatos/Program.cs
+++ TechChallangeCadastroCotatos/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
     .Build();
 
 var jwtKey = Utils.CHAVE_TOKEN;
